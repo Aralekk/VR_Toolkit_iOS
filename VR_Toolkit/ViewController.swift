e519f172a2bb1cb743b07a153f98b294136fd5ca
Arthur Swiniarski <arthurswiniarski@macbook-pro-de-arthur-2.local>
2016-01-19 10:19:57 +0100

    Initial Commit

---
+++ VR_Toolkit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VR_Toolkit
+//
+//  Created by Arthur Swiniarski on 19/01/16.
+//  Copyright © 2016 Arthur Swiniarski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
